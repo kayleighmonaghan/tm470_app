cc5e14c74caa6c7accd6f29cd9b0257853cadc9c
Kayleigh <55881407+kayleighmonaghan@users.noreply.github.com>
2025-06-25 12:16:42 +0100

    added date to home page and installed date formatting package

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    
 
-    return const Center(
-      child: Text('Home Screen', style: TextStyle(fontSize: 24)),
+    return MaterialApp(
+      // browser tab title
+      title: "Home",
+      debugShowCheckedModeBanner: false,
+      // Body
+      home: Scaffold(                                    
+        body: Center(
+          child: Text(
+            // Date format
+            DateFormat.MMMMd().format(DateTime.now()),
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30),
+          ),
+        ),
+      ),
     );
   }
 }
